86248e2873bca4bae5f18cd0b3ba3f1c4fdac2a3
AnnaLesss <klatter@mail.ru>
2022-07-06 11:41:51 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 5000;
+        int bonusMile;
+        bonusMile = ticketPrice / 20;
+
+        System.out.println("Бонусные мили: " + bonusMile);
+    }
+}
