12599e6babef58a4c79106d006ddcc94da637397
AnnaLesss <klatter@mail.ru>
2022-07-07 10:21:21 +0300

    new commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,9 @@
+public class BonusMilesService {
+    public int calculate (int cost) {
+        int price = 5_000;
+        int calculate = price / 20;
+
+        return (cost);
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
+
 public class Main {
     public static void main(String[] args) {
-        int ticketPrice = 5000;
-        int bonusMile;
-        bonusMile = ticketPrice / 20;
-
-        System.out.println("Бонусные мили: " + bonusMile);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
